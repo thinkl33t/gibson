29c277f87432c7710767acd037090ce0d6ab51b9
Kathryn Reeve <Kathryn@binaryKitten.com>
2015-03-28 19:07:24 +0000

    update ldap

--- public/config/autoload/ldap.global.php
+++ public/config/autoload/ldap.global.php
@@ -1,38 +1,13 @@
 <?php
 
-$dbhost = 'localhost';
-$dbname = 'gibson';
-$dbport = '3306';
-$username = $password = 'hacman';
-
-if(isset($_SERVER['RDS_HOSTNAME'])) {
-
-    $dbhost = $_SERVER['RDS_HOSTNAME'];
-    $dbport = $_SERVER['RDS_PORT'];
-    $dbname = $_SERVER['RDS_DB_NAME'];
-
-    $username = $_SERVER['RDS_USERNAME'];
-    $password = $_SERVER['RDS_PASSWORD'];
-}
-
 return [
-    'db' => [
-        'driver'         => 'Pdo',
-        'dsn'            => $dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname}",
-        'driver_options' => [
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
-        ],
-        'username' => $username,
-        'password' => $password
-    ],
-    'service_manager' => [
-        'factories' => [
-            'Zend\Db\Adapter\Adapter'
-            => 'Zend\Db\Adapter\AdapterServiceFactory',
-        ],
-    ],
-    'migrations' => [
-        'dir' => __DIR__ . '/../../migrations',
-        'namespace' => 'Impact\Migration'
+    'ldap' => [
+        'host' => '127.0.0.1',
+        'username' => 'CN=administrator,CN=Users,DC=hackspace,DC=internal',
+        'password' => 'Password4LDAP',
+        'bindRequiresDn' => false,
+        'baseDn' => 'DC=hackspace,DC=internal',
+        'accountCanonicalForm' => \Zend\Ldap\Ldap::ACCTNAME_FORM_PRINCIPAL,
+        'accountDomainName' => 'hackspace.internal',
     ]
 ];
