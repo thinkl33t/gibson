12f69e307c10c0c97555ca338b1bd3317634a894
Bob Clough <bob@clough.me>
2015-03-28 19:47:01 +0000

    cleanup of samba files to enable LDAPS

--- puphpet/files/exec-once/samba.sh
+++ puphpet/files/exec-once/samba.sh
@@ -2,10 +2,17 @@
 sudo apt-get -t wheezy-backports install -y samba ldb-tools ldap-utils
 sudo rm /etc/samba/smb.conf
 sudo samba-tool domain provision --use-rfc2307 --realm hackspace.internal --domain hackspace --server-role=dc --dns-backend=SAMBA_INTERNAL
-#sudo samba-tool domain passwordsettings set --complexity=off
 sudo samba-tool user setpassword administrator --newpassword="Password4LDAP"
 sudo ldbadd -H /var/lib/samba/private/sam.ldb /vagrant/ldap/rfidCode.ldif --option="dsdb:schema update allowed"=true
 sudo ldbmodify -H /var/lib/samba/private/sam.ldb /vagrant/ldap/modUser.ldif --option="dsdb:schema update allowed"=true
 sudo /etc/init.d/samba restart
+
+# rebuild ldap.conf, setting up LDAPs by default
+echo "BASE dc=hackspace,dc=internal" | sudo tee /etc/ldap/ldap.conf
+echo "URI ldaps://localhost:636" | sudo tee -a /etc/ldap/ldap.conf
+echo "TLS_CACERT /etc/ssl/certs/ca-certificates.crt" | sudo tee -a /etc/ldap/ldap.conf
+echo "TLS_REQCERT never" | sudo tee -a /etc/ldap/ldap.conf
+
+# create a test user, and reset their password to Password4LDAP
 sudo ldapadd -w Password4LDAP -D 'administrator@hackspace.internal' -f /vagrant/ldap/createUser.ldif -c
 sudo samba-tool user setpassword demouser --newpassword="Password4LDAP"
